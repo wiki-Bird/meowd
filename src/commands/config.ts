f0a293784b0cf81327d22352af50024db878db84
wiki-Bird <41028518+wiki-Bird@users.noreply.github.com>
2024-07-25 12:08:11 +1200

    ⭐ Fixed emoji regex, starboard

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -290,7 +290,7 @@ const config: Command = {
 				const emote = interaction.options.getString("emoji", true);
 
 				const emoteRegex = /<a?:\w+:\d+>/;
-				const emojiRegex = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E6}-\u{1F1FF}\u{1F201}-\u{1F251}\u{1F004}\u{1F0CF}\u{1F18E}\u{1F191}-\u{1F19A}\u{1F1E6}-\u{1F1FF}]+/gu;
+				const emojiRegex = /^(\p{Emoji_Presentation}|\p{Emoji}\uFE0F)$/u;
 				let valToSave;
 
 				// First try to match a Discord emote
